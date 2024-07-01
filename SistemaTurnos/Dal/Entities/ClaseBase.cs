b591257d28acb26436fec5b6c83aa12704b455b0
sebast825 <sebamolina825@gmail.com>
2024-06-30 21:38:16 -0300

    claseBase

---
+++ SistemaTurnos/Dal/Entities/ClaseBase.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PatronRepositorio_UnitOfWork.Dal.Entities
+{
+    public class ClaseBase
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        //public bool IsDeleted { get; set; }
+        //public DateTime DeletedTime { get; set; }
+
+    }
+}
